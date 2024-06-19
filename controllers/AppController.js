ff8c42c426c46a1761ffb13d6af8eedb273334f9
Margaret-KE <wanjirumargaret394@gmail.com>
2024-06-19 11:56:16 -0700

    AuthController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,17 +1,17 @@
 // Request Handing
 import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+import dbClientInstance from '../utils/db';
 
 export default class AppController {
   static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+      db: dbClientInstance.isAlive(),
     });
   }
 
   static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+    Promise.all([dbClientInstance.nbUsers(), dbClientInstance.nbFiles()])
       .then(([usersCount, filesCount]) => {
         res.status(200).json({ users: usersCount, files: filesCount });
       });
