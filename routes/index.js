9046d9cb540de1a3ab3064caa4f4fa55add6682b
AnyumbaKE <esanyumba@gmail.com>
2024-06-21 20:58:08 +0300

    eslint error fix

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ import FilesController from '../controllers/FilesController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 import { APIError, errorResponse } from '../middlewares/error';
 
+/**
+ * Injects routes with their handlers to the given Express application.
+ * @param {Express} api
+ */
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
